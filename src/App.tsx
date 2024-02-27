a5c36824a4308562b0a8a24cdba63c98b9f96e7d
parkSeryu <pjh971212@gmail.com>
2024-02-27 23:04:08 +0900

    feat: 버튼에 기능개발을 해보자 & 리액트 state변경하는 법

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,35 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [글제목A] = useState('남자 코트 추천');
-  const [글제목B] = useState('강남 우동맛집');
-  const [글제목C] = useState('파이썬독학');
+  const [글제목, 글제목변경] = useState(['남자코트 추천', '강남 우동맛집', '파이썬 독학']);
+  const [따봉] = useState(0);
+
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 blog</div>
       </div>
       <div className="list">
-        <h4>{글제목A}</h4>
+        <h4>
+          {글제목[0]}
+
+          {/* eslint-disable-next-line react/button-has-type */}
+          <button onClick={() => {
+            글제목변경(['여자코트 추천', '강남 우동맛집', '파이썬 독학']);
+          }}
+          >
+            😊
+          </button>
+          {따봉}
+        </h4>
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
-        <h4>{글제목B}</h4>
+        <h4>{글제목[1]}</h4>
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
-        <h4>{글제목C}</h4>
+        <h4>{글제목[2]}</h4>
         <p>2월 17일 발행</p>
       </div>
     </div>

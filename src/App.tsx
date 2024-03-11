c6c1b1f29a46b81cb00cb938f008d75f9503665d
parkSeryu <pjh971212@gmail.com>
2024-03-11 22:33:36 +0900

    feat: Component : 많은 div들을 한 단어로 줄이고 싶으면

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
 import React, { useState } from 'react';
 import './App.css';
 
+function Modal() {
+  return (
+    <div className="modal">
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  );
+}
+
 function App() {
   const [글제목, 글제목변경] = useState(['남자코트 추천', '강남 우동맛집', '파이썬 독학']);
   const [따봉] = useState(0);
@@ -22,7 +32,8 @@ function App() {
       <button
         type="button"
         onClick={() => {
-          const copy = [...글제목]; copy[0] = '여자코트 추천';
+          const copy = [...글제목];
+          copy[0] = '여자코트 추천';
           글제목변경(copy);
         }}
       >
@@ -44,6 +55,7 @@ function App() {
         <h4>{글제목[2]}</h4>
         <p>2월 17일 발행</p>
       </div>
+      <Modal />
     </div>
   );
 }
